abdf4146c926a350ad6be54ded5302be4d21a02e
wesleykam <wesley_kam@ucsb.edu>
2022-11-09 23:49:46 -0800

    fix styling

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.js
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 // import MenuItemReviewsTable from 'main/components/MenuItemReviews/MenuItemReviewsTable';
-import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
+// import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function MenuItemReviewsIndexPage() {
 
