8159156c7cff3c4d703578ad3bdda9e4bf7e8bb5
kaiechen <kaiechen@ucsb.edu>
2022-11-10 22:20:34 -0800

    fixed the REAL silly mistakes

--- frontend/src/tests/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage.test.js
+++ frontend/src/tests/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage.test.js
@@ -1,5 +1,5 @@
 // import { fireEvent, render, waitFor } from "@testing-library/react";
-import { render } from "@testing-library/react"ss
+import { render } from "@testing-library/react"
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import DiningCommonsMenuItemIndexPage from "main/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage";
