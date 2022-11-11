dbfd01d27085de0b3d47ec6191d2db6b9b744b7e
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-11-10 18:09:59 -0800

    ew - fix style

--- frontend/src/main/components/Articles/ArticlesTable.js
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -1,7 +1,7 @@
 import OurTable, { ButtonColumn } from "main/components/OurTable";
 import { useBackendMutation } from "main/utils/useBackend";
 import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/ArticleUtils"
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
 export default function ArticlesTable({ articles, currentUser }) {
