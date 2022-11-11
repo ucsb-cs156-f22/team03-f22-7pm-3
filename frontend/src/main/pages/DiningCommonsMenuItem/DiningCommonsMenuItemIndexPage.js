75a2ec2751721d82af342610bf96b72395f0317a
kaiechen <kaiechen@ucsb.edu>
2022-11-10 21:49:19 -0800

    fixed file name

--- frontend/src/main/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage.js
+++ frontend/src/main/pages/DiningCommonsMenuItem/DiningCommonsMenuItemIndexPage.js
@@ -1,10 +1,10 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function TodosIndexPage() {
+export default function DiningCommonsMenuItemIndexPage() {
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Todos</h1>
+        <h1>Dining Commons Menu Item</h1>
         <p>
           This is where the index page will go
         </p>
