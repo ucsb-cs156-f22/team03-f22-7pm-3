8e8f771ccb06f2dd91958c8431a3ba56a131db36
minhbui27 <63686864+minhbui27@users.noreply.github.com>
2022-11-11 01:57:43 -0800

    Create the placeholder for the Recommendations endpoint (#39)
    * Added the Recommendation tab in the navbar
    
    * Added admin in Recommendation within Navbar and added tests for admin access
    
    * Hopefully fixed mutation testing coverage errors
    
    * resolved merge conflict
    
    * for some reason i had articles in my branch, and no tests, deleting articles
    
    * for some reason also had helprequests in navbar.js, removed
    
    * added placeholder for recommendations pages
    
    * deleted redundant files
    
    * fixed npm coverage
    
    * cleaning commit history
    
    * fixed axios error for code coverage
    
    * added import for RecommendationsIndexPage
    
    * Revert the changes to main
    
    * added hdiningcommonsmenuitems getById method
    
    * added back the menuitemreviews and helprequests in navbar
    
    * added test for diningcommons to pass mutation test
    
    * Removed duplicate helprequests test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,13 +17,16 @@ import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
 import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
 import UCSBDatesEditPage from "main/pages/UCSBDates/UCSBDatesEditPage";
 
+import RecommendationIndexPage from "main/pages/Recommendations/RecommendationsIndexPage";
 import HelpRequsestsIndexPage from "main/pages/HelpRequests/HelpRequestsIndexPage";
 
 /* import RecommendationIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage"; */
 /* import RecommendationCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage"; */
 /* import RecommendationEditPage from "main/pages/UCSBDates/UCSBDatesEditPage"; */
 
+
 import MenuItemReviewsIndexPage from "main/pages/MenuItemReviews/MenuItemReviewsIndexPage";
+
 
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
@@ -85,6 +88,14 @@ function App() {
             <>
               <Route exact path="/ucsbdates/create" element={<UCSBDatesCreatePage />} />
               <Route exact path="/ucsbdates/edit/:id" element={<UCSBDatesEditPage />} />
+            </>
+          )
+        }
+		{/* Creating pages for recommendation */}
+        {
+          hasRole(currentUser, "ROLE_USER") && (
+            <>
+              <Route exact path="/recommendations/list" element={<RecommendationIndexPage/>} />
             </>
           )
         }
