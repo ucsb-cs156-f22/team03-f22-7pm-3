e567a0925e9a198a9585622f0955d27d9c762c0c
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-11-12 14:18:04 -0800

    Add storybook components

---
+++ frontend/src/stories/components/Articles/ArticlesTable.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import ArticlesTable from "main/components/Articles/ArticlesTable";
+import { articlesFixtures } from 'fixtures/articlesFixtures';
+import {diningCommonsFixtures} from "../../../fixtures/diningCommonsFixtures";
+import {currentUserFixtures} from "../../../fixtures/currentUserFixtures";
+
+export default {
+    title: 'components/Articles/ArticlesTable',
+    component: ArticlesTable
+};
+
+const Template = (args) => {
+    return (
+        <ArticlesTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    articles: []
+};
+
+export const ThreeArticles = Template.bind({});
+
+ThreeArticles.args = {
+    articles: articlesFixtures.threeArticles
+};
+
+export const ThreeArticlesAsAdmin = Template.bind({});
+
+ThreeArticlesAsAdmin.args = {
+    articles: articlesFixtures.threeArticles,
+    currentUser: currentUserFixtures.adminUser
+};
+
