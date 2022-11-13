3560323e53502f8b7159cc43246ae4c5c850a861
Ian Lai <ilai@ucsb.edu>
2022-11-13 00:30:03 -0800

    IL - added table to the index page

--- frontend/src/main/components/HelpRequests/HelpRequestsTable.js
+++ frontend/src/main/components/HelpRequests/HelpRequestsTable.js
@@ -1,20 +1,20 @@
-import OurTable, { _ButtonColumn } from "main/components/OurTable";
-import { _useBackendMutation } from "main/utils/useBackend";
-import { _onDeleteSuccess } from "main/utils/UCSBDateUtils";
-// import { hasRole } from "main/utils/currentUser";
-// import { cellToAxiosParamsDelete } from "main/utils/HelpRequestUtils";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import { onDeleteSuccess } from "main/utils/UCSBDateUtils";
+import { hasRole } from "main/utils/currentUser";
+import { cellToAxiosParamsDelete } from "main/utils/HelpRequestUtils";
 
-export default function HelpRequestsTable({ requests, _currentUser }) {
+export default function HelpRequestsTable({ requests, currentUser }) {
     // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/HelpRequest/all"]
-    // );
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/HelpRequest/all"]
+    );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -48,16 +48,16 @@ export default function HelpRequestsTable({ requests, _currentUser }) {
         }
     ];
 
-    // const columnsIfAdmin = [
-    //     ...columns,
-    //     ButtonColumn("Delete", "danger", deleteCallback, "HelpRequestsTable")
-    // ];
+    const columnsIfAdmin = [
+        ...columns,
+        ButtonColumn("Delete", "danger", deleteCallback, "HelpRequestsTable")
+    ];
 
-    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
     return <OurTable
         data={requests}
-        columns={columns}
+        columns={columnsToDisplay}
         testid={"HelpRequestsTable"}
     />;
 };
