cd9fe10cd2fc4bf53a1ed71c2273887318820c72
kaiechen <kaiechen@ucsb.edu>
2022-11-13 00:24:46 -0800

    Help me

--- frontend/src/tests/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable.test.js
+++ frontend/src/tests/components/DiningCommonsMenuItem/DiningCommonsMenuItemTable.test.js
@@ -101,9 +101,9 @@ describe("DiningCommonsMenuItemTable tests", () => {
     expect(editButton).toBeInTheDocument();
     expect(editButton).toHaveClass("btn-primary");
 
-    // const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
-    // expect(deleteButton).toBeInTheDocument();
-    // expect(deleteButton).toHaveClass("btn-danger");
+    const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    expect(deleteButton).toBeInTheDocument();
+    expect(deleteButton).toHaveClass("btn-danger");
 
   });
 
