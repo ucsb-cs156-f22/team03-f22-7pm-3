823e34bddc01bab3fc457bf02543609654feca2f
blakerandle <72828312+blakerandle@users.noreply.github.com>
2022-11-13 14:02:38 -0800

    Add delete button for organization table (#58)
    * br - add organization navbar option with list and create dropdown options
    
    * br - add tests for organization navbar
    
    * br - add placeholder page for organzations list
    
    * br - add tests for placeholder page
    
    * br - edit test imports to correct file path and remove unused
    
    * br - fix capitalization on import in organization index test
    
    * br - create organization table
    
    * br - create organization table storybooks
    
    * br - fix typo on Full Name column in organization table
    
    * br - remove delete button. will address in later issue
    
    * br - increase timeout duration for mutation testing
    
    * br - add organization table to index page
    
    * br - add tests for organization index page with table
    
    * br - add delete button
    
    * br - add test for delete button

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -1,20 +1,18 @@
 import OurTable, { ButtonColumn} from "main/components/OurTable";
-//import { useBackendMutation } from "main/utils/useBackend";
-//import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
+import { useBackendMutation } from "main/utils/useBackend";
+import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
 import { useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
-/*
 export function cellToAxiosParamsDelete(cell) {
     return {
         url: "/api/UCSBOrganization",
         method: "DELETE",
         params: {
-            code: cell.row.values.orgCode
+            id: cell.row.values.orgCode
         }
     }
 }
-*/
 
 export default function UCSBOrganizationTable({ UCSBOrganizations, currentUser }) {
 
@@ -25,15 +23,15 @@ export default function UCSBOrganizationTable({ UCSBOrganizations, currentUser }
     }
 
     // Stryker disable all : hard to test for query caching
-    //const deleteMutation = useBackendMutation(
-    //    cellToAxiosParamsDelete,
-    //    { onSuccess: onDeleteSuccess },
-    //    ["/api/UCSBOrganization/all"]
-    //);
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/UCSBOrganization/all"]
+    );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    //const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
         {
@@ -61,7 +59,7 @@ export default function UCSBOrganizationTable({ UCSBOrganizations, currentUser }
     const columnsIfAdmin = [
         ...columns,
         ButtonColumn("Edit", "primary", editCallback, testid),
-        //ButtonColumn("Delete", "danger", deleteCallback, testid)
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
     ];
 
     const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
